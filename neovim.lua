0dc263a14449597b6ed9ec49ca2113c7bcf2d429
Benja Aguilar <benjaaguilar02@gmail.com>
2025-12-19 14:34:13 -0300

    feat: make changez

--- neovim.lua
+++ neovim.lua
@@ -1,31 +1,15 @@
 return {
-  {
-    "gthelding/monokai-pro.nvim",
-    config = function()
-      require("monokai-pro").setup({
-        filter = "ristretto",
-        override = function()
-          return {
-            NonText = { fg = "#948a8b" },
-            MiniIconsGrey = { fg = "#948a8b" },
-	    MiniIconsRed = { fg = "#fd6883" },
-	    MiniIconsBlue = { fg = "#85dacc" },
-	    MiniIconsGreen = { fg = "#adda78" },
-	    MiniIconsYellow = { fg = "#f9cc6c" },
-	    MiniIconsOrange = { fg = "#f38d70" },
-	    MiniIconsPurple = { fg = "#a8a9eb" },
-	    MiniIconsAzure = { fg = "#a8a9eb" },
-	    MiniIconsCyan = { fg = "#85dacc" }, -- same value as MiniIconsBlue for consistency
-          }
-        end,
-      })
-      vim.cmd([[colorscheme monokai-pro]])
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "monokai-pro",
-    },
-  },
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark_vivid").setup({})
+			vim.cmd([[colorscheme onedark_vivid]])
+		end,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "onedark_vivid",
+		},
+	},
 }
